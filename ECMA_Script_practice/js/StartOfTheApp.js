70ae962be8c65a5d70426367f84875a3bdea10a9
Anhelina <kulakovaanhelina@gmail.com>
2020-09-26 17:22:41 +0200

    classes

--- ECMA_Script_practice/js/StartOfTheApp.js
+++ ECMA_Script_practice/js/StartOfTheApp.js
@@ -11,10 +11,11 @@
 // e.g. class Bill could extend class Cost (with e.g. dueDate and isPaid)
 
 // daily, weekly, monthly, yearly
-const costSpan = [{ span: "Daily", days: 1 },
-{ span: "Weekly", days: 7 },
-{ span: "Monthly", days: 30.45 },
-];
+const costSpan =
+  [{ span: "Daily", days: 1 },
+  { span: "Weekly", days: 7 },
+  { span: "Monthly", days: 30.45 },
+  ];
 
 const costs = [{ name: "Rent", amount: 270.25, span: costSpan[2] },
 { name: "Food", amount: 7.50, span: costSpan[0] },
@@ -45,28 +46,107 @@ console.log(shortWords.some((word) => {
 }));
 
 console.log(shortWords);
-console.log(shortWords.every(word => word.length < 6) )
+console.log(shortWords.every(word => word.length < 6))
 // cheking the findIndex method
 
-const animals = ['hippo', 'tiger', 'lion', 'seal', 'cheetah', 'monkey', 'salamander', 'elephant'];
-const foundAnimal = animals.findIndex(animal => {
-  return animal === 'elephant'
+const dinner = ['salmon', 'beef', 'spagetti', 'pork'];
+const foundAnimal = animals.findIndex(fish => {
+  return dinner === 'fish'
 })
 
-const startsWithS = animals.findIndex(animal => {
+const startsWithS = dinner.findIndex(animal => {
   return animal.charAt(0) === 's'
 })
 
 // checking the reduce method
 const newNumbers = [1, 3, 5, 7];
-const newSum =  newNumbers.reduce((accumulator, currentValue) =>{
+const newSum = newNumbers.reduce((accumulator, currentValue) => {
   console.log('The value of accumulator: ', accumulator);
-console.log('The value of currentValue: ', currentValue);
-return accumulator + currentValue
+  console.log('The value of currentValue: ', currentValue);
+  return accumulator + currentValue
 
-},  10)
+}, 10)
 console.log(newSum);
 
+// object destructoring
+const order = {
+  drink: 'Juice',
+  numberOfGuests: 2,
+  mainCourse: [
+    "pasta carbora", "salmon"
+  ],
+  antipasta: "cheese",
+  cost: 100
+
+}
+
+function bill({ numberOfGuests, cost }) {
+  return `Number of guest: ${numberOfGuests},  price : ${cost}`
+}
+
+console.log(bill(order))
+
+// classes
+class Order {
+  constructor(mainCourse, dessert, appetizer, drinks) {
+    this.mainCourse = mainCourse;
+    this.dessert = dessert;
+    this.appetizer = appetizer;
+    this.drinks = drinks;
+  }
+  myOrder() {
+    console.log(`${this.mainCourse}, ${dessert}, ${this.appetizer}, ${this.drinks}`)
+  }
+}
+
+let firstOrder = new Order("salmon with patato", "tiramisu", "cheese plate", "vine")
+console.log(firstOrder)
+console.log(firstOrder.myOrder)
+class Bill extends Order {
+
+  constructor() {
+    super("beef", "brouni", "meat plate", "vine")
+  }
+
+}
+
+let myBill = Bill()
 
 
+// getters and setters
+let dinner = {
+  _listDinner: [],
+  _listCost: [],
+  set addMeal(name) {
+    this._listDinner.push(name)
+  },
+  get listDinner() {
+    return this._listDinner.join(", ")
+  },
+  set addPrice(cost) {
+    this._listCost.push(cost)
+  }
+,
+  get listCost(){
+    return this._listCost.join(", ")
+  }
+}
 
+dinner.addMeal = "eggs + cabbage";
+dinner.addPrice = 30;
+console.log(dinner._listCost)
+console.log(dinner._listDinner)
+
+let customer = {
+  name: "Alan",
+  orders: ["salmon", "salad", "tea"],
+  printPrefference: function () {
+    let _this = this;
+    this.hobbies.forEach(function (hobby) {
+      let string =`${_this.first} likes to order ${orders}`;
+      console.log(string)
+    })
+  }
+}
+
+customer.printPrefference()
